08c12e68aaf75f1c86b738dd792990ca63c09496
thiago <thiisantos00@gmil.com>
2025-10-22 20:23:20 -0300

    ajuste para banco de dados tabela usuarios

--- extensao/app/Models/Usuario.php
+++ extensao/app/Models/Usuario.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    //
+    protected $table = 'usuarios';
 }
--- extensao/routes/web.php
+++ extensao/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\Usuario;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/usuarios', function () {
+    return Usuario::get();
 });
