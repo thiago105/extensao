0242886a811fbbe3bf918a481c6efa0fb9e5dcb6
thiago <thiisantos00@gmil.com>
2025-10-22 22:08:15 -0300

    atualizado model ponto de coleta

--- extensao/database/migrations/2025_10_23_005026_create_ponto_de_coletas_table.php
+++ extensao/database/migrations/2025_10_23_005026_create_ponto_de_coletas_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
 
             $table->foreign('id_instituicao')
             ->references('id')
-            ->on('instituicao')
+            ->on('instituicaos')
             ->onDelete('cascade');
         });
     }
