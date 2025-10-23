2a32efbe73e931baa9165b4d4b13f83e30d45cda
thiago <thiisantos00@gmil.com>
2025-10-22 21:27:48 -0300

    adicionado rota materiais coletados

--- extensao/routes/web.php
+++ extensao/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Models\Instituicao;
 use App\Models\Usuario;
+use App\Models\Instituicao;
+use App\Models\Mateirais_coletado;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,4 +16,8 @@ Route::get('/usuarios', function () {
 
 Route::get('/instituicaos', function () {
     return Instituicao::get();
+});
+
+Route::get('/materiaisColetados', function () {
+    return Mateirais_coletado::get();
 });
